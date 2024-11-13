7ffc9ebefd694f01fbde627fbae26c78c0981955
Gulrez Alam <egulrezalam@gmail.com>
2024-11-14 03:35:37 +0530

    fixed some minor issue related to deployment

--- car_management/settings.py
+++ car_management/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-kgkzjf4%jw#m*-^q%l7c^q*m%4*wh_-9f@n^@u-t9d21r94r+i"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
